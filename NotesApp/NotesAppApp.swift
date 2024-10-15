360242ba316aeb8c93216f05a734e0805cf209c3
IsabelQuijada <miqc07@outlook.com>
2024-10-15 19:22:02 +0200

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Isabel Quijada on 15.10.24.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
